b697cb2c583b8cc1b61317c8ed303b4c006897ea
AJ Caldwell <tomatrow@gmail.com>
2021-06-04 00:02:15 -0700

    Now with cors

--- functions/market-cap/index.js
+++ functions/market-cap/index.js
@@ -4,6 +4,11 @@ exports.handler = async function () {
     try {
         return {
             statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE"
+            },
             body: JSON.stringify(await fetchMarketCap())
         }
     } catch (error) {
